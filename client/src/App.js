2dcabccba8833ba1608d40cf47829b18700921ce
Cynthia Rittenbach <crittenbach@gmail.com>
2018-07-13 22:19:48 -0400

    moved Bbc news to containers folder

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import {
   Route
 } from 'react-router-dom';
 import logo from './logo.svg';
-import bbcNews from './components/bbcNews.js'
+import bbcNews from './containers/bbcNews.js'
 import './App.css';
 
 class App extends Component {
