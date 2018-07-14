160d8ca81b6a0ffbb75e55a1a4129d0164d5a5ce
Cynthia Rittenbach <crittenbach@gmail.com>
2018-07-14 02:42:12 -0400

    deleted cnn

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import bbcNews from './bbcNewsReducer.js';
 import foxNews from './foxNewsReducer.js';
+import cnnNews from './foxNewsReducer.js';
 
 
 export default combineReducers({
