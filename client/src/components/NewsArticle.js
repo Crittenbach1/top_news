e53dda8e709233909e3a1c1b1287ddce5d86e795
Cynthia Rittenbach <crittenbach@gmail.com>
2018-10-10 18:33:54 -0400

    add button to clickEvent function that will save an article to the api

--- client/src/components/NewsArticle.js
+++ client/src/components/NewsArticle.js
@@ -3,13 +3,22 @@ var $ = require ('jquery');
 
 class NewsArticle extends React.Component {
 
-      render() {
+
+
+      handleClick(event) { 
+          alert("hello");
+      }
+
+
+
+       render() {
 
         return (
           <div className="article">
             <img alt = "" src={this.props.article.urlToImage}/>
              <h3><a href={this.props.article.url}>{this.props.article.title}</a></h3>
              <p>{this.props.article.description}</p>
+             <input type="submit" onClick={this.handleClick.bind(this)} value="Save Article"/>
           </div>
         );
        }
