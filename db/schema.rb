cc81d3057b954ad0b99a41c608cb18e37b70840d
Cynthia Rittenbach <crittenbach@gmail.com>
2018-10-10 17:52:22 -0400

    replace article table with articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_14_001828) do
+ActiveRecord::Schema.define(version: 2018_10_10_212038) do
 
-  create_table "article", force: :cascade do |t|
+  create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.string "description"
