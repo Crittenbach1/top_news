ce1f1a7799247d4f3ceb8189daff6846f3c8c583
Cynthia Rittenbach <crittenbach@gmail.com>
2018-10-10 18:44:48 -0400

    add savedArticles reducer to index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 import bbcNews from './bbcNewsReducer.js';
 import foxNews from './foxNewsReducer.js';
-import cnnNews from './foxNewsReducer.js';
+import savedArticles from './savedArticlesReducer.js';
 
 
 export default combineReducers({
   bbcNews,
   foxNews,
-  cnnNews,
+  savedArticles,
 });
