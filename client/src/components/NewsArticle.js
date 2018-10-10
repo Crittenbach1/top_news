d0903250e997dfddcb9031a0740dbf47be2ed574
Cynthia Rittenbach <crittenbach@gmail.com>
2018-10-10 18:36:20 -0400

    assign article props to variables before ajax

--- client/src/components/NewsArticle.js
+++ client/src/components/NewsArticle.js
@@ -5,8 +5,13 @@ class NewsArticle extends React.Component {
 
 
 
-      handleClick(event) { 
-          alert("hello");
+      handleClick(event) {
+        var title = this.props.article.title;
+        var author = this.props.article.author;
+        var description = this.props.article.description;
+        var url = this.props.article.url;
+
+        
       }
 
 
@@ -18,6 +23,7 @@ class NewsArticle extends React.Component {
             <img alt = "" src={this.props.article.urlToImage}/>
              <h3><a href={this.props.article.url}>{this.props.article.title}</a></h3>
              <p>{this.props.article.description}</p>
+             <br/>
              <input type="submit" onClick={this.handleClick.bind(this)} value="Save Article"/>
           </div>
         );
