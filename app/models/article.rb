5b70cde62eeb0bce70b90cb0461e9d486c0c44e5
Cynthia Rittenbach <crittenbach@gmail.com>
2018-10-10 18:55:31 -0400

    delete presence validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,4 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :url, presence: true
-  validates :author, presence: true
 
 
 
