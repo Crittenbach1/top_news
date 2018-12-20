24415db8cca0e1edbe978317cd6eba8e2a70554d
Cynthia Rittenbach <crittenbach@gmail.com>
2018-12-20 02:33:55 -0500

    the saved news article component contains a like button that calls likeArticle action

--- client/src/components/SavedNewsArticle.js
+++ client/src/components/SavedNewsArticle.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class SavedNewsArticle extends Component {
+
+  constructor(props){
+
+    super(props);
+
+  }
+
+  updateStateLikes = () => {
+
+       var newArticle = {
+         id: this.props.article.id,
+         title: this.props.article.title,
+         url: this.props.article.url,
+         author: this.props.article.author,
+         description: this.props.article.description,
+         likes: this.props.article.likes + 1
+       };
+       this.props.likeArticle(newArticle);
+
+   }
 
 
-const SavedNewsArticle = ( { article } ) =>
+  render() {
 
-      <div>
-         <h3><a href={article.url}>{article.title}</a></h3>
-         <p>{article.author}</p>
-         <p>{article.description}</p>
-      </div>
+      return (
+        <div>
+           <h3><a href={this.props.article.url}>{this.props.article.title}</a></h3>
+           <p>{this.props.article.author}</p>
+           <p>{this.props.article.description}</p>
+           <p>likes: {this.props.article.likes}</p>
+           <button onClick={() => {
+            this.updateStateLikes();
+           }}> Like </button>
+        </div>
+      );
+  }
 
+}
 
 export default SavedNewsArticle;
