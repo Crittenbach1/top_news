68347474cb12275b747b29ae1153fdef55fa8d72
Cynthia Rittenbach <crittenbach@gmail.com>
2018-12-20 02:35:43 -0500

    the savedArticle container passes the likeArticle action to the savedArticle component through props

--- client/src/containers/savedArticles.js
+++ client/src/containers/savedArticles.js
@@ -2,22 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Header from '../components/Header.js'
 import SavedNewsArticle from '../components/SavedNewsArticle.js'
+import { likeArticle } from '../actions/likeArticleActions.js'
+import { fetchSavedArticles } from '../actions/savedArticlesActions.js';
 
 import '../App.css';
 
 class SavedArticles extends Component {
 
 
-  render() {
-      return (
 
+  render() {
+
+      return (
           <div>
            <Header title="Saved News Articles" />
-            {this.props.articles.map(article=><SavedNewsArticle key={article.id} article={article} />)}
+            {this.props.articles.map(article=><SavedNewsArticle key={article.id} article={article} likeArticle={this.props.likeArticle} />)}
           </div>
-
       );
-
   }
 }
 
@@ -26,4 +27,5 @@ const mapStateToProps = (state) => {
 }
 
 
-export default connect(mapStateToProps)(SavedArticles);
+
+export default connect(mapStateToProps, {likeArticle, fetchSavedArticles})(SavedArticles);
